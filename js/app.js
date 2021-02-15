5344651d6300a3422b7ae0793bb2d18e6ae3b68f
Reema Eilouti <reema.eilouti@gmail.com>
2021-02-15 13:27:21 +0200

    changed all objects to instances of the constructor function

--- js/app.js
+++ js/app.js
@@ -2,229 +2,63 @@
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-
-let seattle = {
-    name: 'Seattle',
-    minimumCustomersPerHour: 0,
-    maximumCustomersPerHour: 0,
-    averageCookiesPerCustomer: 0,
-    setMinimumCustomersPerHour: function (min, max) { this.minimumCustomersPerHour = getRandomNumber(min, max); },
-    setMaximumCustomersPerHour: function (min, max) { this.maximumCustomersPerHour = getRandomNumber(min, max); },
-    setAverageCookiesPerCustomer: function (min, max) { this.averageCookiesPerCustomer = getRandomNumber(min, max); },
-    render: function () {
-        let container = document.getElementById('Branches');
-
-        let h2El = document.createElement('h2');
-        container.appendChild(h2El);
-        h2El.textContent = this.name;
-
-        let ulEl = document.createElement('ul');
-        container.appendChild(ulEl);
-
-        let totalCookies = 0;
-
-        for (let i = 06; i < 20; i++) {
-            let liEl = document.createElement('li');
-            ulEl.appendChild(liEl);
-
-            this.setMinimumCustomersPerHour(0, 5);
-            this.setMaximumCustomersPerHour(6, 20);
-            this.setAverageCookiesPerCustomer(1, 5);
-
-            averageCustomersPerHour = (this.maximumCustomersPerHour - this.minimumCustomersPerHour) / 2;
-
-            averageCookiesPerHour = Math.floor(averageCustomersPerHour * this.averageCookiesPerCustomer);
-
-            totalCookies += averageCookiesPerHour;
-
-            liEl.textContent = `${i}:00 -> ${averageCookiesPerHour} cookies.`;
-        }
-
-        let liEl = document.createElement('li');
-        ulEl.appendChild(liEl);
-        liEl.textContent = `Total: ${totalCookies} cookies.`;
-
-    },
 };
 
 
-let tokyo = {
-    name: 'Tokyo',
-    minimumCustomersPerHour: 0,
-    maximumCustomersPerHour: 0,
-    averageCookiesPerCustomer: 0,
-    setMinimumCustomersPerHour: function (min, max) { this.minimumCustomersPerHour = getRandomNumber(min, max); },
-    setMaximumCustomersPerHour: function (min, max) { this.maximumCustomersPerHour = getRandomNumber(min, max); },
-    setAverageCookiesPerCustomer: function (min, max) { this.averageCookiesPerCustomer = getRandomNumber(min, max); },
-    render: function () {
-        let container = document.getElementById('Branches');
+function Branch(name) {
+    this.name = name;
+    this.minimumCustomersPerHour = 0;
+    this.maximumCustomersPerHour = 0;
+    this.averageCookiesPerCustomer = 0;
+};
 
-        let h2El = document.createElement('h2');
-        container.appendChild(h2El);
-        h2El.textContent = this.name;
+Branch.prototype.setMinimumCustomersPerHour = function (min, max) { this.minimumCustomersPerHour = getRandomNumber(min, max); };
 
-        let ulEl = document.createElement('ul');
-        container.appendChild(ulEl);
+Branch.prototype.setMaximumCustomersPerHour = function (min, max) { this.maximumCustomersPerHour = getRandomNumber(min, max); };
 
-        let totalCookies = 0;
+Branch.prototype.setAverageCookiesPerCustomer = function (min, max) { this.averageCookiesPerCustomer = getRandomNumber(min, max); };
 
-        for (let i = 06; i < 20; i++) {
-            let liEl = document.createElement('li');
-            ulEl.appendChild(liEl);
+Branch.prototype.render = function () {
+    let container = document.getElementById('Branches');
 
-            this.setMinimumCustomersPerHour(0, 5);
-            this.setMaximumCustomersPerHour(6, 20);
-            this.setAverageCookiesPerCustomer(1, 5);
+    let h2El = document.createElement('h2');
+    container.appendChild(h2El);
+    h2El.textContent = this.name;
 
-            averageCustomersPerHour = (this.maximumCustomersPerHour - this.minimumCustomersPerHour) / 2;
+    let ulEl = document.createElement('ul');
+    container.appendChild(ulEl);
 
-            averageCookiesPerHour = Math.floor(averageCustomersPerHour * this.averageCookiesPerCustomer);
+    let totalCookies = 0;
 
-            totalCookies += averageCookiesPerHour;
-
-            liEl.textContent = `${i}:00 -> ${averageCookiesPerHour} cookies.`;
-        }
-
+    for (let i = 06; i < 20; i++) {
         let liEl = document.createElement('li');
         ulEl.appendChild(liEl);
-        liEl.textContent = `Total: ${totalCookies} cookies.`;
-    },
+
+        this.setMinimumCustomersPerHour(0, 5);
+        this.setMaximumCustomersPerHour(6, 20);
+        this.setAverageCookiesPerCustomer(1, 5);
+
+        averageCustomersPerHour = (this.maximumCustomersPerHour - this.minimumCustomersPerHour) / 2;
+
+        averageCookiesPerHour = Math.floor(averageCustomersPerHour * this.averageCookiesPerCustomer);
+
+        totalCookies += averageCookiesPerHour;
+
+        liEl.textContent = `${i}:00 -> ${averageCookiesPerHour} cookies.`;
+    }
+
+    let liEl = document.createElement('li');
+    ulEl.appendChild(liEl);
+    liEl.textContent = `Total: ${totalCookies} cookies.`;
+
 };
 
 
-let dubai = {
-    name: 'Dubai',
-    minimumCustomersPerHour: 0,
-    maximumCustomersPerHour: 0,
-    averageCookiesPerCustomer: 0,
-    setMinimumCustomersPerHour: function (min, max) { this.minimumCustomersPerHour = getRandomNumber(min, max); },
-    setMaximumCustomersPerHour: function (min, max) { this.maximumCustomersPerHour = getRandomNumber(min, max); },
-    setAverageCookiesPerCustomer: function (min, max) { this.averageCookiesPerCustomer = getRandomNumber(min, max); },
-    render: function () {
-        let container = document.getElementById('Branches');
-
-        let h2El = document.createElement('h2');
-        container.appendChild(h2El);
-        h2El.textContent = this.name;
-
-        let ulEl = document.createElement('ul');
-        container.appendChild(ulEl);
-
-        let totalCookies = 0;
-
-        for (let i = 06; i < 20; i++) {
-            let liEl = document.createElement('li');
-            ulEl.appendChild(liEl);
-
-            this.setMinimumCustomersPerHour(0, 5);
-            this.setMaximumCustomersPerHour(6, 20);
-            this.setAverageCookiesPerCustomer(1, 5);
-
-            averageCustomersPerHour = (this.maximumCustomersPerHour - this.minimumCustomersPerHour) / 2;
-
-            averageCookiesPerHour = Math.floor(averageCustomersPerHour * this.averageCookiesPerCustomer);
-
-            totalCookies += averageCookiesPerHour;
-
-            liEl.textContent = `${i}:00 -> ${averageCookiesPerHour} cookies.`;
-        }
-
-        let liEl = document.createElement('li');
-        ulEl.appendChild(liEl);
-        liEl.textContent = `Total: ${totalCookies} cookies.`;
-    },
-};
-
-
-let paris = {
-    name: 'Paris',
-    minimumCustomersPerHour: 0,
-    maximumCustomersPerHour: 0,
-    averageCookiesPerCustomer: 0,
-    setMinimumCustomersPerHour: function (min, max) { this.minimumCustomersPerHour = getRandomNumber(min, max); },
-    setMaximumCustomersPerHour: function (min, max) { this.maximumCustomersPerHour = getRandomNumber(min, max); },
-    setAverageCookiesPerCustomer: function (min, max) { this.averageCookiesPerCustomer = getRandomNumber(min, max); },
-    render: function () {
-        let container = document.getElementById('Branches');
-
-        let h2El = document.createElement('h2');
-        container.appendChild(h2El);
-        h2El.textContent = this.name;
-
-        let ulEl = document.createElement('ul');
-        container.appendChild(ulEl);
-
-        let totalCookies = 0;
-
-        for (let i = 06; i < 20; i++) {
-            let liEl = document.createElement('li');
-            ulEl.appendChild(liEl);
-
-            this.setMinimumCustomersPerHour(0, 5);
-            this.setMaximumCustomersPerHour(6, 20);
-            this.setAverageCookiesPerCustomer(1, 5);
-
-            averageCustomersPerHour = (this.maximumCustomersPerHour - this.minimumCustomersPerHour) / 2;
-
-            averageCookiesPerHour = Math.floor(averageCustomersPerHour * this.averageCookiesPerCustomer);
-
-            totalCookies += averageCookiesPerHour;
-
-            liEl.textContent = `${i}:00 -> ${averageCookiesPerHour} cookies.`;
-        }
-
-        let liEl = document.createElement('li');
-        ulEl.appendChild(liEl);
-        liEl.textContent = `Total: ${totalCookies} cookies.`;
-    },
-};
-
-
-let lima = {
-    name: 'Lima',
-    minimumCustomersPerHour: 0,
-    maximumCustomersPerHour: 0,
-    averageCookiesPerCustomer: 0,
-    setMinimumCustomersPerHour: function (min, max) { this.minimumCustomersPerHour = getRandomNumber(min, max); },
-    setMaximumCustomersPerHour: function (min, max) { this.maximumCustomersPerHour = getRandomNumber(min, max); },
-    setAverageCookiesPerCustomer: function (min, max) { this.averageCookiesPerCustomer = getRandomNumber(min, max); },
-    render: function () {
-        let container = document.getElementById('Branches');
-
-        let h2El = document.createElement('h2');
-        container.appendChild(h2El);
-        h2El.textContent = this.name;
-
-        let ulEl = document.createElement('ul');
-        container.appendChild(ulEl);
-
-        let totalCookies = 0;
-
-        for (let i = 06; i < 20; i++) {
-            let liEl = document.createElement('li');
-            ulEl.appendChild(liEl);
-
-            this.setMinimumCustomersPerHour(0, 5);
-            this.setMaximumCustomersPerHour(6, 20);
-            this.setAverageCookiesPerCustomer(1, 5);
-
-            averageCustomersPerHour = (this.maximumCustomersPerHour - this.minimumCustomersPerHour) / 2;
-
-            averageCookiesPerHour = Math.floor(averageCustomersPerHour * this.averageCookiesPerCustomer);
-
-            totalCookies += averageCookiesPerHour;
-
-            liEl.textContent = `${i}:00 -> ${averageCookiesPerHour} cookies.`;
-        }
-
-        let liEl = document.createElement('li');
-        ulEl.appendChild(liEl);
-        liEl.textContent = `Total: ${totalCookies} cookies.`;
-    },
-};
-
+let seattle = new Branch('Seattle');
+let tokyo = new Branch('Tokyo');
+let dubai = new Branch('Dubai');
+let paris = new Branch('Paris');
+let lima = new Branch('Lima');
 
 seattle.render();
 tokyo.render();
